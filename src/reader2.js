b871a1deefb4d99382f3d6b79ba2ab675e958653
Tuan Anh Tran <me@tuananh.org>
2022-01-15 15:36:54 +0700

    wip
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- src/reader2.js
+++ src/reader2.js
@@ -12,13 +12,14 @@ function getQuerystringByName(name) {
 }
 
 function isValidURL(url) {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-  return !!pattern.test(url);
+  // const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+  //   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+  //   '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+  //   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+  //   '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+  //   '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  // return !!pattern.test(url);
+  return true;
 }
 
 function copyShareURL() {
